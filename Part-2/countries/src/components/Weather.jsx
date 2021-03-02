0e4407532e8ea7cc5d9f0ccef53fbddb2af4f22c
Josephine19001 <gyameraj@gmail.com>
2021-03-02 18:14:38 +0200

    modified app component to render description only

--- Part-2/countries/src/components/Weather.jsx
+++ Part-2/countries/src/components/Weather.jsx
@@ -4,12 +4,7 @@ export default function Weather({ city, current }) {
   return (
     <div>
       <h2>Weather in {city}</h2>
-      <p>Temperature: {current.temperature}</p>
-      <p>population: {current.population}</p>
-      <div>
-        <img style={{ width: "50%" }} src={current.weather_icons} />
-      </div>
-      <p>wind: {current.wind}</p>
+      <p>Temperature: {current.description}</p>
     </div>
   );
 }
