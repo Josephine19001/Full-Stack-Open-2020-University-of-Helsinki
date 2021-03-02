7bb970fcaf04751812687eae7fbf6ae108ef08d3
Josephine19001 <gyameraj@gmail.com>
2021-03-02 18:14:21 +0200

    refactored country file

--- Part-2/countries/src/components/Country.jsx
+++ Part-2/countries/src/components/Country.jsx
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Weather from './Weather';
+import keys from "../keys";
 
 export default function Country({ country }) {
+  const [isShow, setIsShow] = useState(false);
+  const [weatherData, setWeatherData] = useState([]);
+
+  const weatherBaseURL = `http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${keys.openWeatherApiKey}`;
+
+  const handleShow = (event,country) => {
+    event.preventDefault();
+    setIsShow(!isShow);
+  };
+
+  useEffect(() => {
+    const fetchWeatherData = () => {
+      axios
+        .get(weatherBaseURL)
+        .then((response) => setWeatherData([...response.data.weather]));
+        
+    };
+    fetchWeatherData();
+  }, []);
+
+  console.log('weather', weatherData);
+
   return (
     <div>
       <h2>{country.name}</h2>
-      <p>capital {country.capital}</p>
-      <p>population {country.population}</p>
+      <span>
+        {" "}
+        <button onClick={handleShow}>show</button>
+      </span>
+      {isShow && (
+        <>
+          <p>capital {country.capital}</p>
+          <p>population {country.population}</p>
 
-      <div>
-        <h3>languages</h3>
-        <div>
-          <ul>
-            {country.languages.map((lang) => {
-             return <li>{lang.name}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <div>
-        <img style={{ width: "50%" }} src={country.flag} />
-      </div>
+          <div>
+            <h3>languages</h3>
+            <div>
+              <ul>
+                {country.languages.map((lang) => {
+                  return <li>{lang.name}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div>
+            <img style={{ width: "50%" }} src={country.flag} />
+          </div>
+          {
+            weatherData.map(current => {
+              <Weather current={current} city={country.capital} />
+            })
+          }
+        </>
+      )}
     </div>
   );
 }
