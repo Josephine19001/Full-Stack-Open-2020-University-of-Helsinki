0164b7f520bcf3029aa34d67b0e3abae3d397e6c
Josephine19001 <gyameraj@gmail.com>
2021-05-27 20:54:19 +0300

    Fixed linting issues

--- Part-3/models/phonebook.js
+++ Part-3/models/phonebook.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const phoneSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: [8, "name should now be less than 8"],
-      required: [true, "name is required"],
-      unique: true
-    },
-    number: {
-      type: String,
-      minLength: [10, "number should now be less than 10"],
-      required: [true, "number is required"]
-    },
-});
+  name: {
+    type: String,
+    minLength: [8, 'name should now be less than 8'],
+    required: [true, 'name is required'],
+    unique: true
+  },
+  number: {
+    type: String,
+    minLength: [10, 'number should now be less than 10'],
+    required: [true, 'number is required']
+  },
+})
 
 phoneSchema.set('toJSON', {
   transform: (document, returnedObject) => {
@@ -22,4 +22,4 @@ phoneSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Phone', phoneSchema);
+module.exports = mongoose.model('Phone', phoneSchema)
