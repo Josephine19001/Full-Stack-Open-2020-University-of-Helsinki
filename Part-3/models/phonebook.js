3c87fb993bb314f6d2043b82f177bece658d1846
Josephine19001 <gyameraj@gmail.com>
2021-05-27 20:42:18 +0300

    Add mongoose validation

--- Part-3/models/phonebook.js
+++ Part-3/models/phonebook.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const phoneSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+      type: String,
+      minLength: [8, "name should now be less than 8"],
+      required: [true, "name is required"],
+      unique: true
+    },
+    number: {
+      type: String,
+      minLength: [10, "number should now be less than 10"],
+      required: [true, "number is required"]
+    },
 });
 
 phoneSchema.set('toJSON', {
