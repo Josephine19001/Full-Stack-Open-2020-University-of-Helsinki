85e0cf36a594178e058b299e66689f3f28395e90
Josephine19001 <gyameraj@gmail.com>
2021-06-01 08:13:52 +0300

    Add controller router

---
+++ Part-4/controllers/blog.js
@@ -0,0 +1,19 @@
+const Blog = require("../models/blog");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Blog.find({}).then((blogs) => {
+    res.json(blogs);
+  });
+});
+
+router.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save().then((result) => {
+    res.status(201).json(result);
+  });
+});
+
+module.exports = router;
